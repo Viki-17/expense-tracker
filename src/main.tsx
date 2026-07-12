58415b4c7c31ae7941554a957a85189c947270a9
Vignesh Ravichandar <vigneshchandar.r@gmail.com>
2026-07-12 16:50:03 +0530

    Revert "Use relative asset paths (./) instead of absolute"
    This reverts commit da62c5da8f861247c720c7611dca2574c5c108fb.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-const routerBase = import.meta.env.VITE_ROUTER_BASE || '/';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={routerBase}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
